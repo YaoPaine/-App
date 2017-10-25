9c190ec480ffa963666bafa059c2c52459620b7e
YaoPaine <yaohe1991@gmail.com>
2017-10-25 20:08:07 +0800

    先说坑：1、module D已经添加了butterknife 的annotationProcessor，却还是无法使用，删除新建后又可以用了，不知道怎么回事（期间各种clean，restart） 2、module C明明有/modulec/build/generated/source/apt/release/com/alibaba/android/arouter/routes/ARouter$$Group$$c.java 这个生成的类，却还报错，clean后就好了 再来总结：1、纠正自己的一个错误，一开始看别人的博客，说“更建议把图片、 strings、 colors、dimens 等资源放到 resourc 这个module中 去，可以防止不同的资源名字却对应了同一资源值。”，这样做确实可以防止这个问题，当时误以为也可以把layout 等文件放到resource module中，后来删除代码后思考到，layout放在相应module中，更有利于管理（删除了activity，相应的layout就可以删掉，不需要改layout）。当然这里保留了layout放在resource中的做法，也新增了layout 直接放在相应module的做法。strings 资源同理（待验证）

--- RxJavaAndRetrofit/moduled/src/main/java/com/yao/moduled/ModuleDActivity.java
+++ RxJavaAndRetrofit/moduled/src/main/java/com/yao/moduled/ModuleDActivity.java
@@ -1,17 +1,39 @@
 package com.yao.moduled;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.yao.lib_mvp.BaseActivity;
 import com.yao.resource.constants.RouterConstants;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by heyao on 2017/10/25.
+ */
+
 @Route(path = RouterConstants.MODULE_D_ACTIVITY)
-public class ModuleDActivity extends AppCompatActivity {
+public class ModuleDActivity extends BaseActivity {
+
+    @BindView(R2.id.module_d_tv)
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.module_d_activity_main);
+        setContentView(R.layout.module_d_main_activity);
+    }
+
+    @OnClick({R2.id.module_d_tv})
+    public void clickEvent(View view) {
+        int id = view.getId();
+        if (R.id.module_d_tv == id) {
+            ARouter.getInstance().build(RouterConstants.MODULE_A_ACTIVITY).navigation();
+        }
     }
 }
