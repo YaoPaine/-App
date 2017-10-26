bf7f5ae4d74022b48d674ff0bc781c6009fad297
YaoPaine <yaohe1991@gmail.com>
2017-10-26 17:03:29 +0800

    将三方库放入lib-common中

--- RxJavaAndRetrofit/app/src/main/java/com/yao/rxjavaandretrofit/MainActivity.java
+++ RxJavaAndRetrofit/app/src/main/java/com/yao/rxjavaandretrofit/MainActivity.java
@@ -10,6 +10,8 @@ import com.yao.lib_mvp.BaseActivity;
 import com.yao.lib_mvp.R2;
 import com.yao.resource.constants.RouterConstants;
 
+import java.util.Locale;
+
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
@@ -18,6 +20,24 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Locale[] availableLocales = Locale.getAvailableLocales();
+        for (Locale availableLocale : availableLocales) {
+            try {
+                Log.e(TAG, "getCountry()：" + availableLocale.getCountry() +
+                        "\tgetDisplayCountry()：" + availableLocale.getDisplayCountry() +
+                        "\tgetDisplayLanguage()：" + availableLocale.getDisplayLanguage() +
+                        "\tgetDisplayName()" + availableLocale.getDisplayName() +
+                        "\tgetLanguage()：" + availableLocale.getLanguage() +
+                        "\tgetISO3Country()：" + availableLocale.getISO3Country() +
+                        "\tgetISO3Language()：" + availableLocale.getISO3Language() +
+                        "\tgetDisplayVariant()：" + availableLocale.getDisplayVariant()
+                );
+            } catch (Exception e) {
+                Log.e(TAG, "onCreate: " + e);
+            }
+
+        }
     }
 
     @OnClick({R2.id.button1, R2.id.button2, R2.id.button3})
@@ -35,7 +55,7 @@ public class MainActivity extends BaseActivity {
                 intent.putExtra("module", "my name is he yao");
                 startActivity(intent);*/
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("user", new UserModel("heyao","!@#qwe123"));
+                bundle.putSerializable("user", new UserModel("heyao", "!@#qwe123"));
                 ARouter.getInstance().build(RouterConstants.MODULE_A_ACTIVITY).with(bundle).navigation();
                 break;
             case R.id.button3:
